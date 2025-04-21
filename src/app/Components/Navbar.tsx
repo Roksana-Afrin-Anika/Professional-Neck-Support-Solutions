af9f8d2aacd118280b52910281b68728981779c0
Roksana-Afrin-Anika <roksanaafrin47@gmail.com>
2025-04-21 20:55:08 +0600

    Fix import path casing for Vercel build

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Menu, X, Search, User } from "lucide-react";
-import AuthModal from "../Components/AuthModel";
+import AuthModal from "./AuthModel";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
